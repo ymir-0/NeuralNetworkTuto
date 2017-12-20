b0d469a1e0699d24e1b47084a9ebc2faec725e43
ymir <mazieres.pierre@gmail.com>
2017-12-20 23:31:45 +0100

    test logical functions

--- neuralnetworktuto/1_artificialNeuron.py
+++ neuralnetworktuto/1_artificialNeuron.py
@@ -1,3 +1,49 @@
 #!/usr/bin/env python3
 # see : https://www.anyflo.com/bret/cours/rn/rn3.htm
 # imports
+from numpy import heaviside, array, transpose, newaxis
+# constants
+WA=-.75 # weight for input 'a'
+WB=-.75 # weight for input 'b'
+T=-1 # threshold
+THRESHOLD=array((WA,WB,T))
+# neuron
+def neuron(a,b):
+    # sum weighted input
+    INPUT_ARRAY=array((a,b,-1))
+    WEIGHTED_INPUT=THRESHOLD.dot(INPUT_ARRAY.transpose())
+    # compute & return OUT
+    OUTPUT=heaviside(WEIGHTED_INPUT, 0)
+    return OUTPUT
+# logical functions
+nand = lambda a, b : neuron(a,b)
+not_ = lambda a : nand(a,a)
+and_ = lambda a, b : nand(nand(a,b),nand(a,b))
+or_ = lambda a, b : nand(not_(a),not_(b))
+nor_ = lambda a, b : not_(or_(a,b))
+xor = lambda a, b : and_(or_(a,b),not_(and_(a,b)))
+# ... we can build all logical functions
+# test
+print("nand 0 0 : " + str(nand(0,0)))
+print("nand 0 1 : " + str(nand(0,1)))
+print("nand 1 0 : " + str(nand(1,0)))
+print("nand 1 1 : " + str(nand(1,1)))
+print("not 0 : " + str(not_(0)))
+print("not 1 : " + str(not_(1)))
+print("and 0 0 : " + str(and_(0,0)))
+print("and 0 1 : " + str(and_(0,1)))
+print("and 1 0 : " + str(and_(1,0)))
+print("and 1 1 : " + str(and_(1,1)))
+print("or 0 0 : " + str(or_(0,0)))
+print("or 0 1 : " + str(or_(0,1)))
+print("or 1 0 : " + str(or_(1,0)))
+print("or 1 1 : " + str(or_(1,1)))
+print("nor 0 0 : " + str(nor_(0,0)))
+print("nor 0 1 : " + str(nor_(0,1)))
+print("nor 1 0 : " + str(nor_(1,0)))
+print("nor 1 1 : " + str(nor_(1,1)))
+print("xor 0 0 : " + str(xor(0,0)))
+print("xor 0 1 : " + str(xor(0,1)))
+print("xor 1 0 : " + str(xor(1,0)))
+print("xor 1 1 : " + str(xor(1,1)))
+pass
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# import generic modules
+from os.path import join, dirname, isdir
+from setuptools import setup
+from importlib import import_module
+from pkgutil import walk_packages
+# parse recursively a module
+# this code is an adaptation from https://stackoverflow.com/questions/3365740/how-to-import-all-submodules
+def import_submodules(package):
+    if isinstance(package, str):
+        package = import_module(package)
+    modules = list()
+    for loader, name, is_pkg in walk_packages(package.__path__):
+        full_name = package.__name__ + "." + name
+        full_path=join(loader.path,name)
+        # continue recursion only if module is folder
+        if is_pkg and isdir(full_path):
+            modules.append(full_name)
+            modules=modules+(import_submodules(full_name))
+    return modules
+# import and parse dedicated module
+import neuralnetworktuto
+rootPackage=neuralnetworktuto
+module_path = join(dirname(__file__), rootPackage.__name__ , "version.py")
+version_line = [line for line in open(module_path) if line.startswith("__version__")][0]
+__version__ = version_line.split("__version__ = ")[-1][1:][:-2]
+modules=[rootPackage.__name__]+import_submodules (rootPackage)
+print("loaded modules : " + str(modules))
+# define setup parameters
+setup(
+    name="NeuralNetworkTuto",
+    version=__version__,
+    description="Neural network tutorial",
+    packages=modules,
+    install_requires=["numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
