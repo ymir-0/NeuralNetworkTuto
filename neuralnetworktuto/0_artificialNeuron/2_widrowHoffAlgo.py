284b806ab15c6cc6bbc4e985b4a5035b4e3b90bb
ymir <mazieres.pierre@gmail.com>
2018-01-03 16:18:00 +0100

    better random initialization

--- neuralnetworktuto/0_artificialNeuron/2_widrowHoffAlgo.py
+++ neuralnetworktuto/0_artificialNeuron/2_widrowHoffAlgo.py
@@ -2,15 +2,33 @@
 # imports
 from numpy import heaviside, array, append
 from numpy.random import rand
+from os import linesep, sep
+from os.path import join, realpath
+# contants
+CURRENT_DIRECTORY = realpath(__file__).rsplit(sep, 1)[0]
+# tools class
+class Logger():
+    completeLog=""
+    @staticmethod
+    def append(level, message):
+        Logger.completeLog=Logger.completeLog+" "*(4*level)+message+linesep
+    @staticmethod
+    def flush():
+        logFile = open(join(CURRENT_DIRECTORY,"training.log"),"wt")
+        logFile.write(Logger.completeLog)
+        logFile.close()
 # neurons
 class Neuron():
-    computeLimitLoop=10000 # sometimes, random choices are too long to adjust. better to retry
-    def __init__(self,trainings,adjustmentStep):
+    computeLimitLoop=20 # sometimes, random choices are too long to adjust. better to retry
+    def __init__(self,trainings):
         # initiate neuron
-        weightsNumber=len(tuple(trainings.keys())[0]) # we use the length of 1 expected data key to know weights are required
-        weights=rand(weightsNumber)
-        threshold=rand(1) # we assume threshold to be an weight so it can be adjust and condidere 'global threshold'=0
+        adjustmentStep=.01
+        weightsNumber=len(tuple(trainings.keys())[0]) # we use the length of 1st training data to get neurons number
+        # INFO : found with a dichotomy between 1 and 0 for doing some loops
+        threshold=rand()*.6+.1
+        weights=rand(weightsNumber)*.1
         self.thresholdedWeights=append(weights,-threshold)
+        Logger.append(0,"initial weights / threashold : " + str(self.thresholdedWeights))
         # adjust neuron
         adjusted=False
         actualLoopNumber=0
@@ -32,11 +50,18 @@ class Neuron():
                     thresholdedInputs = append(inputs, 1)
                     for neuronIndex, neuronActivated in enumerate(thresholdedInputs):
                         if neuronActivated==1:
-                            self.thresholdedWeights[neuronIndex]=self.thresholdedWeights[neuronIndex]+adjustmentSigne*adjustmentStep
+                            self.thresholdedWeights[neuronIndex]=self.thresholdedWeights[neuronIndex]+adjustmentStep
             # check loops number
             actualLoopNumber = actualLoopNumber + 1
             if actualLoopNumber >= Neuron.computeLimitLoop:
-                raise Exception("Sorry, random choices are too long to adjust. Better to retry")
+                message="Sorry, random choices are too long to adjust. Better to retry"
+                Logger.append(0, message)
+                Logger.flush()
+                raise Exception(message)
+        # print completed training
+        Logger.append(0,"trained in "+str(actualLoopNumber) + " steps :"+linesep+str(self.thresholdedWeights))
+        Logger.flush()
+        pass
     def activate(self,inputs):
         # sum weighted input
         thresholdedInputs = append(inputs, 1)
@@ -51,7 +76,7 @@ class Neuron():
     ((1, 0)): 0,
     ((1, 1)): 1,
 }
-andNeuron=Neuron(trainings,0.1)
+andNeuron=Neuron(trainings)
 print("and 0 0 : " + str(andNeuron.activate((0,0))))
 print("and 0 1 : " + str(andNeuron.activate((0,1))))
 print("and 1 0 : " + str(andNeuron.activate((1,0))))
@@ -63,7 +88,7 @@ trainings={
     ((1, 0)): 1,
     ((1, 1)): 1,
 }
-andNeuron=Neuron(trainings,0.1)
+andNeuron=Neuron(trainings)
 print("or 0 0 : " + str(andNeuron.activate((0,0))))
 print("or 0 1 : " + str(andNeuron.activate((0,1))))
 print("or 1 0 : " + str(andNeuron.activate((1,0))))
