9d61649f4f1457d3fee6a654b1f7bb1e9751108a
ymir <mazieres.pierre@gmail.com>
2018-01-11 22:44:48 +0100

    try output correction #3

--- neuralnetworktuto/1_perceptron/2_multiLayer_binaryPicture/0_testMatrices.py
+++ neuralnetworktuto/1_perceptron/2_multiLayer_binaryPicture/0_testMatrices.py
@@ -18,21 +18,17 @@ class Sigmoid():
     pass
 # perceptron
 class Perceptron():
-    def __init__(self,layerHeights,weightLimit=0.125,thresholdLimit=0.125):
+    def __init__(self,layerHeights,weightLimit=0.125):
         # initialize attributs
         self.weights=list()
-        self.thresholds=list()
         # for each layer
-        for layerIndex in range(1,len(layerHeights)): #INFO : there is no weights/thresholds related to input layer
+        for layerIndex in range(1,len(layerHeights)): #INFO : there is no weights related to input layer
             # get heights for current & previous layers
             currentHeight = layerHeights[layerIndex]
             previousHeight = layerHeights[layerIndex-1]
             # randomize layer weights
             layerWeights=(rand(currentHeight,previousHeight)-.5)*2*weightLimit
             self.weights.append(layerWeights)
-            # randomize layer thresholds
-            threshold=(rand(currentHeight)-.5)*2*thresholdLimit
-            self.thresholds.append(threshold)
             pass
         pass
     pass
@@ -44,35 +40,28 @@ class Perceptron():
         # initialize current layer input
         currentInput=input
         # for each layer
-        for layerIndex in range(1, len(self.weights)+1): #INFO : there is no weights/thresholds related to input layer
+        for layerIndex in range(1, len(self.weights)+1): #INFO : there is no weights related to input layer
             # get activation inputs
-            currentActivationInput = self.activationInputs(currentInput, layerIndex)
-            currentActivationOutput = self.activationResults(currentActivationInput, layerIndex, training)
+            layerOutput = self.activateLayer(currentInput, layerIndex, True)
             # compute & memorize sigmoid input (if training)
             if training:
-                self.outputs.append(currentActivationOutput)
+                self.outputs.append(layerOutput)
             # next layer input is current layer outpout
-            currentInput = currentActivationOutput
+            currentInput = layerOutput
             pass
-        result = currentActivationOutput
+        result = layerOutput
         return result
     # activation input : A = sum(W*I)
-    def activationInputs(self, input, layerIndex):
+    def activateLayer(self, input, layerIndex, training = False):
         layerWeights = self.weights[layerIndex-1] #INFO : there is no weights related to input layer
-        activationInputs = layerWeights.dot(input)
-        return activationInputs
-    # activation result : O = sigmoïd(A-threshold)
-    def activationResults(self, activationInput, layerIndex, training = False):
-        # get thresholds
-        layerThreshold = self.thresholds[layerIndex-1] #INFO : there is no thresholds related to input layer
         # compute & memorize sigmoid input (if training)
-        aggregation= activationInput - layerThreshold
+        aggregation = layerWeights.dot(input)
         if training:
             self.aggregations.append(aggregation)
         # activate layer
-        activationResults = Sigmoid.value(aggregation)
+        output = Sigmoid.value(aggregation)
         # return
-        return activationResults
+        return output
     # correct output layer : error = sigmoide'(E) * ( T - S )
     def outputError(self,expectedOutput): # INFO : T=expectedOutput ; pas=correctionStep
         # compute weights correction step
